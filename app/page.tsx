3c0907002a39b710c6d4a9419e08d9601e35be21
Luiz Fernando <luizfernandoandradedequeiroz@gmail.com>
2026-05-26 18:59:16 -0300

    cabeçalho

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,30 @@
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator"
+import { Plus, List, Check, X } from 'lucide-react'
+import { Badge } from "@/components/ui/badge"
+
+
+
 const Home = () => {
   return (
-    <div>Luiz</div>
+    <main className="w-full h-screen bg-gray-400 flex justify-center items-center">
+      <Card className="w-lg p-5">
+        <CardHeader className="flex gap-2">
+          <input className="w-[289] p-2" placeholder="Adicionar Tarefa" />
+          <Button className="ml-auto cursor-pointer"><Plus />Cadastrar</Button>
+        </CardHeader>
+        <CardContent>
+          <Separator />
+          <div className="flex gap-2">
+            <Badge className="cursor-pointer"><List />Todas</Badge>
+            <Badge className="cursor-pointer"><X />Nao finalizadas</Badge>
+            <Badge className="cursor-pointer"><Check />Concluidas</Badge>
+          </div>
+        </CardContent>
+      </Card>
+    </main>
   )
 }
 
