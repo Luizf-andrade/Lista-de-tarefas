b081120c74de104b17faa19e11e824acf21fa976
Luiz Fernando <luizfernandoandradedequeiroz@gmail.com>
2026-05-27 15:22:18 -0300

    config buttons excluir tarefa e eitar tarefas

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
-import { Plus, List, Check, X, SquarePen, Trash, ListCheck } from 'lucide-react'
+import { Plus, List, Check, X, SquarePen, Trash, ListCheck, Sigma } from 'lucide-react'
 import { Badge } from "@/components/ui/badge"
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog"
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 
 
 
@@ -31,7 +33,22 @@ const Home = () => {
               <div className="w-2 h-full bg-green-300"></div>
               <p className="flex-1 px-2 text-sm">Estudar React</p>
               <div className="flex gap-2 items-center">
-                <SquarePen size={16} className="cursor-pointer" />
+                <Dialog>
+                  <DialogTrigger>
+                    <SquarePen size={16} className="cursor-pointer" />
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Editar Tarefa</DialogTitle>
+                    </DialogHeader>
+
+                    <div className="flex gap-2">
+                      <Input placeholder="Editar tarefa" />
+                      <Button className="cursor-pointer">Editar</Button>
+                    </div>
+
+                  </DialogContent>
+                </Dialog>
                 <Trash size={16} className="cursor-pointer" />
               </div>
             </div>
@@ -42,18 +59,38 @@ const Home = () => {
               <ListCheck size={15} />
               <p className="text-xs">Tarefas Concluidas (3/3)</p>
             </div>
-            <Button className="text-xs h-7 cursor-pointer" variant="outline"><Trash />Limpar Tarefas Concluidas</Button>
+
+            <AlertDialog>
+              <AlertDialogTrigger>
+                <Button className="text-xs h-7 cursor-pointer" variant="outline"><Trash />Limpar Tarefas Concluidas</Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Tem certeza que deseja excluir x itens?</AlertDialogTitle>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogAction>Sim</AlertDialogAction>
+                  <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+
           </div>
 
-          <div className="h-8 w-full bg-gray-500">
-            <div className="h-2 w-52 bg-blue-500"></div>
+          <div className="h-2 w-full bg-gray-500 mt-4 rounded-md">
+            <div className="h-full bg-blue-500 rounded-md" style={{ width: "50%" }}></div>
+          </div>
+
+          <div className="flex justify-end items-center mt-2 gap-2">
+            <Sigma size={18} />
+            <p className="text-xs">3 tarefas no total</p>
           </div>
         </CardContent>
 
         <div></div>
 
       </Card>
-    </main>
+    </main >
   )
 }
 
